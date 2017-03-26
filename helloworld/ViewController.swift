f627b270f1bb74b55aa2a979baaac95fcfbc3c42
EzgiDurmazpinar <edurmaz1@binghamton.edu>
2017-03-25 22:36:25 -0400

    MyFirst
    My first app :) after starting swift !

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var titleImage: UIImageView!
+    @IBOutlet weak var WelcomeBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func WelcomePressed(_ sender: Any) {
+        background.isHidden = false
+        titleImage.isHidden = false
+        WelcomeBtn.isHidden = true
+    }
 
 }
 
